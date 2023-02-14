45592486b6f07e2521c9d2939344c6fbf9367a63
Samuel Durante <samueldurante.h@gmail.com>
2023-02-14 00:40:20 +0000

    feat(home): render data with cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
+import Cards from '@/components/Card';
+import data from '@/data/data.json';
+
+const Home = () => {
   return (
-    <div className="h-screen">
-      <h1>Hello, World!</h1>
+    <div className="h-screen flex items-center flex-col py-2">
+      <div className="flex flex-col gap-4">
+        {data.map((item, key) => (
+          <Cards key={key} {...item} />
+        ))}
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
