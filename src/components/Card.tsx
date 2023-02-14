4d67a87db09c09da064b51875e60bb7b5d64d962
Samuel Durante <samueldurante.h@gmail.com>
2023-02-14 00:39:37 +0000

    feat(components): add card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,40 @@
+type Props = {
+  institution: string;
+  campus: string;
+  course: string;
+  cutoff_mark: number;
+  group: string;
+};
+
+const Cards = ({ institution, campus, course, cutoff_mark, group }: Props) => {
+  return (
+    <div className="border box-border rounded max-w-4xl">
+      <div className="flex">
+        <div className="grid grid-cols-3 gap-6 p-4">
+          <div className="flex flex-col">
+            <span className="text-xs break-words">Instituição:</span>
+            <span className="text-sm break-words">{institution}</span>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-xs break-words">Campus:</span>
+            <span className="text-sm break-words">{campus}</span>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-xs break-words">Curso:</span>
+            <span className="text-sm break-words">{course}</span>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-xs break-words">Nota de Corte:</span>
+            <span className="text-sm break-words">{cutoff_mark}</span>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-xs break-words">Grupo:</span>
+            <span className="text-sm break-words">{group}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
