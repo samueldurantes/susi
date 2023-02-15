2d37a50c5c1c43e855ee727f94e08ab6f749bb99
Samuel Durante <samueldurante.h@gmail.com>
2023-02-15 21:07:10 +0000

    fix(home): wrong variables

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import CardCourse from '@/components/CardCourse';
 import data from '@/data/data.json';
 
-type ICourseData = {
+type CourseData = {
   institution: string;
   campus: string;
   course: string;
@@ -13,15 +13,46 @@ type ICourseData = {
 };
 
 const Home = () => {
-  const [courses, setCourses] = useState(() => data.slice(0, 5));
   const [currentPage, setCurrentPage] = useState(0);
   const [showLoader, setShowLoader] = useState(true);
-  const loaderRef = useRef(null);
-  
-    const [courses, setCourses] = useState<string[]>([]);
-  const [couresData, setCouresData] = useState<ICourseData[]>(data);
+  const [courses, setCourses] = useState<string[]>([]);
+  const [couresData, setCouresData] = useState<CourseData[]>(data.slice(0, 5));
   const [shifts, setShifts] = useState<string[]>([]);
   const [universities, setUniversities] = useState<string[]>([]);
+  const loaderRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: '20px',
+      threshold: 1.0,
+    };
+
+    const observer = new IntersectionObserver((entities) => {
+      const target = entities[0];
+
+      if (target.isIntersecting) {
+        setCurrentPage((old) => old + 1);
+        setShowLoader(true);
+      }
+    }, options);
+
+    if (loaderRef.current) {
+      observer.observe(loaderRef.current);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (courses.length === data.length) {
+      setShowLoader(false);
+      return;
+    }
+
+    setTimeout(() => {
+      setCouresData((old) => couresData.slice(0, old.length + 5));
+    }, 1500);
+  }, [currentPage, couresData, courses]);
+
   useEffect(() => {
     const dataCourses: string[] = [];
     const dataUniversity: string[] = [];
@@ -67,46 +98,14 @@ const Home = () => {
     }
   };
 
-  useEffect(() => {
-    const options = {
-      root: null,
-      rootMargin: '20px',
-      threshold: 1.0,
-    };
-
-    const observer = new IntersectionObserver((entities) => {
-      const target = entities[0];
-
-      if (target.isIntersecting) {
-        setCurrentPage((old) => old + 1);
-        setShowLoader(true);
-      }
-    }, options);
-
-    if (loaderRef.current) {
-      observer.observe(loaderRef.current);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (courses.length === data.length) {
-      setShowLoader(false);
-      return;
-    }
-
-    setTimeout(() => {
-      setCourses((old) => data.slice(0, old.length + 5));
-    }, 1500);
-  }, [currentPage, courses, data]);
-
   return (
     <div className="h-screen flex items-center flex-col px-6 py-4">
       <div className="flex flex-col max-w-4xl py-4 gap-4">
         <h1 className="text-2xl font-mono">Notas de cortes - ENEM 2022</h1>
         <p>
           Olá! Esse projeto foi criado com intuito de facilitar a visualização
-          das notas de corte do SISU, já que eu tinha curiosidade em saber as notas de
-          corte do ano anterior e o SISU só disponibiliza um arquivo{' '}
+          das notas de corte do SISU, já que eu tinha curiosidade em saber as
+          notas de corte do ano anterior e o SISU só disponibiliza um arquivo{' '}
           <a
             className="text-blue-500 hover:text-blue-700 hover:underline"
             href="https://en.wikipedia.org/wiki/Microsoft_Excel#File_formats"
@@ -165,7 +164,7 @@ const Home = () => {
         </div>
       </div>
       <div className="flex flex-col gap-4">
-        {couresData.map((item: ICourseData, key: number) => (
+        {couresData.map((item: CourseData, key: number) => (
           <CardCourse key={key} {...item} />
         ))}
       </div>
