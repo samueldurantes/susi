0b81534c90d51360665945523135465d8b468e9d
Igor <ig.vp@hotmail.com>
2023-02-15 10:46:08 -0300

    added search input to course

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,40 @@
 import CardCourse from '@/components/CardCourse';
 import data from '@/data/data.json';
+import { useEffect, useState } from 'react';
+
+interface ICourseData {
+  institution: string;
+  campus: string;
+  course: string;
+  cutoff_mark: number;
+  group: string;
+  shift: string;
+}
 
 const Home = () => {
+  const [courses, setCourses] = useState<string[]>([]);
+  const [couresData, setCouresData] = useState<ICourseData[]>(data);
+  useEffect(() => {
+    const dataCourses: string[] = [];
+    data.forEach((item) => {
+      let [courseName] = item.course.split('-');
+      courseName = courseName.substring(0, courseName.length - 1);
+      !dataCourses.includes(courseName) && dataCourses.push(courseName);
+    });
+    setCourses(dataCourses);
+  }, []);
+
+  const handleSelectCourse = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (courses.includes(event.target.value)) {
+      const Data = [...data].filter((item) => {
+        let [courseName] = item.course.split('-');
+        courseName = courseName.substring(0, courseName.length - 1);
+        return courseName === event.target.value;
+      });
+      setCouresData(Data);
+    }
+  };
+
   return (
     <div className="h-screen flex items-center flex-col px-6 py-2">
       <div className="flex flex-col max-w-4xl py-4 gap-4">
@@ -22,8 +55,22 @@ const Home = () => {
           para facilitar a vida de quem também tem essa curiosidade.
         </p>
       </div>
+      <div className="py-2 w-56 ">
+        <input
+          type="text"
+          list="data"
+          placeholder="Todos os cursos"
+          className="border-2 border-gray-50 rounded-md p-1"
+          onChange={(e) => handleSelectCourse(e)}
+        />
+        <datalist id="data">
+          {courses.map((item: string) => {
+            return <option key={item}>{item}</option>;
+          })}
+        </datalist>
+      </div>
       <div className="flex flex-col gap-4">
-        {data.map((item, key) => (
+        {couresData.map((item: ICourseData, key: number) => (
           <CardCourse key={key} {...item} />
         ))}
       </div>
