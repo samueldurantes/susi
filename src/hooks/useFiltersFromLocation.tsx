8a8c146fc79f971f1b11b14e6afc301a5216d283
Samuel Durante <44513615+samueldurantes@users.noreply.github.com>
2023-12-24 14:12:41 -0300

    feat(hooks): add useFiltersFromLocation hook (#11)

---
+++ src/hooks/useFiltersFromLocation.tsx
@@ -0,0 +1,52 @@
+import { useRouter } from 'next/router';
+import queryString from 'query-string';
+
+export const useFiltersFromLocation = () => {
+  const router = useRouter();
+
+  const getFiltersFromLocation = () => {
+    const { query } = router;
+    const { filters } = query;
+
+    if (!filters) {
+      return {};
+    }
+
+    // Parse the filters string from the URL using query-string
+    try {
+      const parsedFilters = queryString.parse(filters);
+      return parsedFilters;
+    } catch (error) {
+      console.error('Error parsing filters from location:', error);
+      return {};
+    }
+  };
+
+  const setFiltersToLocation = (newFilters) => {
+    const { pathname, query } = router;
+    const currentFilters = getFiltersFromLocation();
+
+    // Merge the current filters with the new filters
+    const mergedFilters = {
+      ...currentFilters,
+      ...newFilters,
+    };
+
+    // Convert the filters to a query string
+    const filtersQueryString = queryString.stringify(mergedFilters);
+
+    // Push the updated filters to the URL
+    router.push({
+      pathname,
+      query: {
+        ...query,
+        filters: filtersQueryString,
+      },
+    });
+  };
+
+  return {
+    filters: getFiltersFromLocation(),
+    setFiltersToLocation,
+  };
+};
