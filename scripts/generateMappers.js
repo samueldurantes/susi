5dc72f71fb9b2c8264191c046c296bf791fa49e9
Samuel Durante <44513615+samueldurantes@users.noreply.github.com>
2023-12-25 10:20:27 -0400

    feat(scripts): add more scripts (#15)

---
+++ scripts/generateMappers.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+const json = require('./a.json');
+
+const coursesByInstitution = (data) => {
+  const result = {};
+
+  data.forEach((entry) => {
+    const { institution, course } = entry;
+
+    if (!result[institution]) {
+      result[institution] = [course];
+    } else {
+      result[institution].push(course);
+    }
+  });
+
+  return result;
+};
+
+const institutionByCourses = (data) => {
+  const result = {};
+
+  data.forEach((item) => {
+    const { course, institution } = item;
+
+    if (!result[course]) {
+      result[course] = [institution];
+    } else if (!result[course].includes(institution)) {
+      result[course].push(institution);
+    }
+  });
+
+  return result;
+};
+
+fs.writeFileSync(
+  path.resolve(__dirname, '..', 'src', 'data', `universitiesMapByCourses.json`),
+  JSON.stringify(institutionByCourses(json), null, 2)
+);
+
+fs.writeFileSync(
+  path.resolve(__dirname, '..', 'src', 'data', `coursesMapByUniversities.json`),
+  JSON.stringify(coursesByInstitution(json), null, 2)
+);
